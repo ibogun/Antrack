2c89e40cf2c5c10368999c233845dd5544a5ac24
Ivan <ibogun2010@my.fit.edu>
2015-10-22 16:19:35 -0400

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -13,7 +13,7 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     sudo apt-get install build-essential
     wget http://www.cmake.org/files/v3.2/cmake-3.2.2.tar.gz
     tar xf cmake-3.2.2.tar.gz
-    cd cmake-3.2.2./
+    cd cmake-3.2.2/
     ./configure
     make && sudo make install
     cd ..
