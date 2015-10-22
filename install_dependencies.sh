42ebecb5065a1971fbb9cd18d22ad793022d01ae
Ivan <ibogun2010@my.fit.edu>
2015-10-22 14:14:50 -0400

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -2,7 +2,6 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     
     set -x
     export DEBIAN_FRONTEND=noninteractive
-    add-apt-repository -y ppa:tuleu/precise-backports
     sudo apt-get update && sudo apt-get upgrade -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
     sudo apt-get -qq install libopencv-dev
     sudo apt-get -qq install liblapack-dev
@@ -10,7 +9,31 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     sudo apt-get -qq install libboost-dev
     sudo apt-get -qq install libarmadillo-dev
     sudo apt-get -qq install libboost-all-dev
-    sudo apt-get -qq install libgflags-dev libgoogle-glog-dev
+    # install cmake 3.x
+    sudo apt-get install build-essential
+    wget http://www.cmake.org/files/v3.2/cmake-3.2.2.tar.gz
+    tar xf cmake-3.2.2.tar.gz
+    cd cmake-3.2.2./
+    ./configure
+    make && sudo make install
+    cd ..
+    
+    # install glog
+    wget https://google-glog.googlecode.com/files/glog-0.3.3.tar.gz
+    tar zxvf glog-0.3.3.tar.gz
+    cd glog-0.3.3
+    ./configure
+    make && make install
+    cd ..
+    # gflags
+    wget https://github.com/schuhschuh/gflags/archive/master.zip
+    unzip master.zip
+    cd gflags-master
+    mkdir build && cd build
+    export CXXFLAGS="-fPIC" && /usr/local/bin/cmake .. && make VERBOSE=1
+    make && sudo make install
+    cd ..
+    
     # ...
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     brew update && brew upgrade && brew tap homebrew/science && \
